02d935ced871ad1fa270bfa1afd1eeda7fc345cd
MondoBiscuit <bradleofabian@gmail.com>
2018-08-19 19:15:54 -0400

    fixing bugs

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var PORT = process.env.port || 8080;
+
 var app = express();
 
-app.use(express.static(process.cwd() + '/public'));
+app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-var router = require('./controllers/burgers_controller.js');
-app.use('/', router);
+var router = require('./controllers/burgers_controller');
+app.use(router);
 
-var PORT = process.env.port || 8080;
+
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
